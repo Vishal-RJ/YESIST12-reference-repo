ddcdc03242944480dcb2bf1ca33197d4d2959e39
R J Vishal <vishalonline2023@gmail.com>
2025-10-19 20:28:51 +0530

    Update header.js

--- yesist/components/header.js
+++ yesist/components/header.js
@@ -1,22 +1,29 @@
+// --- DYNAMIC PATH WORKAROUND ---
+// This script checks if the current page is in a subfolder.
+const isTrackPage = window.location.pathname.includes('/tracks/');
+const basePath = isTrackPage ? '../' : '';
+
 const headerHTML = `
 <header class="site-header">
     <div class="container header-container">
         <div class="logo">
-            <a href="yesist_index.html"><img src="images/IEEE-YESIST12-Logo-1024x307-1.png" alt="YESIST12 Logo"></a>
+            <!-- ⭐ FIX: The 'basePath' variable is added to the start of paths -->
+            <a href="${basePath}yesist_index.html"><img src="${basePath}images/IEEE-YESIST12-Logo-1024x307-1.png" alt="YESIST12 Logo"></a>
         </div>
         <nav class="main-nav">
             <ul>
-                <li><a href="yesist_index.html">HOME    </a></li>
-                <li><a href="about-us.html">ABOUT US</a></li>
+                <!-- ⭐ FIX: All paths now use the dynamic 'basePath' variable -->
+                <li><a href="${basePath}yesist_index.html">HOME</a></li>
+                <li><a href="${basePath}about-us.html">ABOUT US</a></li>
                 <li class="has-dropdown">
-                    <a href="#tracks">TRACKS</a>
+                    <a href="#">TRACKS</a>
                     <ul class="dropdown">
-                        <li><a href="innovation_challenge.html">Innovation Challenge</a></li>
-                        <li><a href="junior_einstein.html">Junior Einstein</a></li>
-                        <li><a href="maker_fair.html">Maker Fair</a></li>
-                        <li><a href="wepower.html">WePOWER Special Track</a></li>
-                        <li><a href="special_track.html">Special Track</a></li>
-                        <li><a href="iengage.html">IEngage Track</a></li>
+                        <li><a href="${basePath}tracks/innovtion_challenge.html">Innovation Challenge</a></li>
+                        <li><a href="${basePath}tracks/junior_einstein.html">Junior Einstein</a></li>
+                        <li><a href="${basePath}tracks/maker_fair.html">Maker Fair</a></li>
+                        <li><a href="${basePath}tracks/wepower.html">WePOWER Special Track</a></li>
+                        <li><a href="${basePath}tracks/special_track.html">Special Track</a></li>
+                        <li><a href="${basePath}tracks/iengage.html">IEngage Track</a></li>
                     </ul>
                 </li>
                 <li class="has-dropdown">
@@ -38,7 +45,6 @@ const headerHTML = `
 </header>
 `;
 
-// This safer method adds the header after the page loads
-document.addEventListener('DOMContentLoaded', () => {
-    document.body.insertAdjacentHTML('afterbegin', headerHTML);
-});
+// This inserts the HTML at the top of the <body>
+document.body.insertAdjacentHTML('afterbegin', headerHTML);
+
